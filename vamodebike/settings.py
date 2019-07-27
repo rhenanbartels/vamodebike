68246383f6eeca4825adc121c61631cf0dc83553
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-07-27 10:42:54 -0300

    Create minimal landing page

--- vamodebike/settings.py
+++ vamodebike/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR.child('static'),
+]
